d8c1cba52616a687124081a08bfcaa4f93ccab6c
Berserk29 <oliviertrudeau777@gmail.com>
2023-09-06 19:00:01 -0400

    Worked on the canvas logic, added the collision and the detection of the canvas edge. Added logo on the circle. Need to work on the isHover property and changing the color of the svg

--- src/component/cavas/cavas.component.jsx
+++ src/component/cavas/cavas.component.jsx
@@ -1,6 +1,8 @@
 import { useRef, useEffect } from "react";
 import { Circle } from "./cavas.data";
 
+import svgIcon from '../../assets/icon/Devicon-html5-plain.svg'
+
 const Canvas = ({hoverNum}) => {
     const canvasRef = useRef()
     let canvasCur;
@@ -16,6 +18,33 @@ const Canvas = ({hoverNum}) => {
         // When the distance is smaller or equal to the sum of the two radius, the circles touch or overlap
         return squareDistance <= ((r1 + r2) * (r1 + r2))
     }
+
+    function detectEdgeCollisions()
+    {
+        let obj;
+        for (let i = 0; i < circleArr.length; i++)
+        {
+            obj = circleArr[i];
+   
+            // Check for left and right
+            if (obj.x < obj.radius){
+                obj.vx = Math.abs(obj.vx);
+                obj.x = obj.radius;
+            }else if (obj.x > canvasCur.width - obj.radius){
+                obj.vx = -Math.abs(obj.vx);
+                obj.x = canvasCur.width - obj.radius;
+            }
+   
+            // Check for bottom and top
+            if (obj.y < obj.radius){
+                obj.vy = Math.abs(obj.vy);
+                obj.y = obj.radius;
+            } else if (obj.y > canvasCur.height - obj.radius){
+                obj.vy = -Math.abs(obj.vy);
+                obj.y = canvasCur.height - obj.radius;
+            }
+        }
+   }
 
     const detectCollisions = () => {
         let obj1;
@@ -37,6 +66,20 @@ const Canvas = ({hoverNum}) => {
                 if (circleIntersect(obj1.x, obj1.y, obj1.radius, obj2.x, obj2.y, obj2.radius)){
                     obj1.isColliding = true;
                     obj2.isColliding = true;
+                    let vCollision = {x: obj2.x - obj1.x, y: obj2.y - obj1.y}
+                    let distance = Math.sqrt((obj2.x-obj1.x)*(obj2.x-obj1.x) + (obj2.y-obj1.y)*(obj2.y-obj1.y));
+                    let vCollisionNorm = {x: vCollision.x / distance, y: vCollision.y / distance};
+                    let vRelativeVelocity = {x: obj1.vx - obj2.vx, y: obj1.vy - obj2.vy};
+                    let speed = vRelativeVelocity.x * vCollisionNorm.x + vRelativeVelocity.y * vCollisionNorm.y;
+
+                    if(speed < 0) {
+                        break;
+                    }
+
+                    obj1.vx -= (speed * vCollisionNorm.x);
+                    obj1.vy -= (speed * vCollisionNorm.y);
+                    obj2.vx += (speed * vCollisionNorm.x);
+                    obj2.vy += (speed * vCollisionNorm.y);
                 }
             }
         }
@@ -57,6 +100,7 @@ const Canvas = ({hoverNum}) => {
         }
 
         detectCollisions();
+        detectEdgeCollisions();
         clearCanvas();
 
         for (let i = 0; i < circleArr.length; i++) {
@@ -73,16 +117,16 @@ const Canvas = ({hoverNum}) => {
         canvasCur = canvasRef?.current;
         context = canvasCur.getContext('2d')
         
-            circleArr = [
-            new Circle(context, 250, 50, 0, 50),
-            new Circle(context, 250, 300, 0, -50),
-            new Circle(context, 150, 0, 50, 50),
-            new Circle(context, 250, 150, 50, 50),
-            new Circle(context, 350, 75, -50, 50),
+        circleArr = [
+            new Circle(context, 250, 50, 0, 60, svgIcon),
+            new Circle(context, 250, 300, 0, -60),
+            new Circle(context, 0, 0, 60, 60),
+            new Circle(context, 350, 150, -60, 60),
+            new Circle(context, 600, 0, -60, 60),
         ]
 
         window.requestAnimationFrame(gameLoop)        
-    },[hoverNum])
+    },[])
 
     return (
         <canvas ref={canvasRef} width={650} height={400} style={{border: '1px solid var(--color-primary)'}}/>
--- src/component/cavas/cavas.data.js
+++ src/component/cavas/cavas.data.js
@@ -14,20 +14,40 @@ export class GameObject
 
 export class Circle extends GameObject
 {
-    constructor (context, x, y, vx, vy){
+    constructor (context, x, y, vx, vy, svgIconUrl){
         super(context, x, y, vx, vy);
 
         // Set default width
-        this.radius = 30;
+        this.radius = 45;
+
+        this.svgIconUrl = svgIconUrl;
+
+        if (typeof this.svgIconUrl === 'string') {
+            this.loadSvgIcon(this.svgIconUrl);
+        }
     }
 
+    loadSvgIcon(url) {
+        this.svgIcon = new Image();
+        this.svgIcon.src = this.svgIconUrl;
+        this.svgIcon.onload = () => {
+            this.draw();
+        };
+    }
 
     draw(){
         // Draw a simple Circle
         this.context.beginPath();
-        this.context.fillStyle = this.isColliding || this.isHover ? 'red' : '#568ab8';
+        this.context.fillStyle = this.isHover ? 'purple' : '#568ab8';
         this.context.arc(this.x, this.y, this.radius , 0, 2 * Math.PI)
         this.context.fill();
+
+        if (this.svgIcon instanceof Image && this.svgIcon.complete) {
+            // You can adjust the position and size of the SVG icon as needed
+            const iconWidth = 45;
+            const iconHeight = 45;
+            this.context.drawImage(this.svgIcon, this.x - iconWidth / 2, this.y - iconHeight / 2, iconWidth, iconHeight);
+        }
     }
 
     update(secondsPassed){
